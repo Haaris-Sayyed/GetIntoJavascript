81f6b26cc6e938af6fdf2cd3c293021dcf378eb3
Haaris Sayyed <haaris7200@gmail.com>
2022-04-19 18:20:44 +0530

    Updated Program

--- datatypes.js
+++ datatypes.js
@@ -3,83 +3,83 @@
 let challenge='30 Days Of JavaScript'
 
 // Print the string on the browser console using console.log()
-console.log(`challenge = ${challenge}`)
+console.log(`challenge = ${challenge}`)  // challenge = 30 Days Of JavaScript
 
 // Print the length of the string on the browser console using console.log()
-console.log('length=',challenge.length)
+console.log('length=',challenge.length) // length= 21
 
 // Change all the string characters to capital letters using toUpperCase() method
-console.log('toUpperCase=','30 Days Of JavaScript'.toUpperCase())
+console.log('toUpperCase=','30 Days Of JavaScript'.toUpperCase()) // toUpperCase= 30 DAYS OF JAVASCRIPT
 
 // Change all the string characters to lowercase letters using toLowerCase() method
-console.log('toLowerCase=','30 Days Of JavaScript'.toLowerCase())
+console.log('toLowerCase=','30 Days Of JavaScript'.toLowerCase()) // toLowerCase= 30 days of javascript
 
 // Cut (slice) out the first word of the string using substring() method
 let subStr=challenge.substring(0,2)
-console.log('Substring=',subStr)
+console.log('Substring=',subStr)      // Substring= 30
 
 // Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
-let phrase=challenge.substring(3)
-console.log('phrase=',phrase)
+let phrase=challenge.substring(3)  // phrase= Days Of JavaScript
+console.log('phrase=',phrase) 
 
 // Check if the string contains a word Script using includes() method
-console.log(challenge.includes('Script'))
+console.log(challenge.includes('Script')) // true
 
 // Split the string into an array using split() method
-console.log(challenge.split())
+console.log(challenge.split())  // [ '30 Days Of JavaScript' ]
 
 // Split the string 30 Days Of JavaScript at the space using split() method
-console.log(challenge.split(' '))
+console.log(challenge.split(' ')) // [ '30', 'Days', 'Of', 'JavaScript' ]
 
 // 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
 let string='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
-console.log(string.split(' '))
+console.log(string.split(', '))  // ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
 
 // Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
-console.log('30 Days Of JavaScript'.replace('JavaScript','Python'))
+console.log('30 Days Of JavaScript'.replace('JavaScript','Python')) // 30 Days Of Python
 
 // What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
-console.log(challenge.charAt(15))
+console.log(challenge.charAt(15))  // S
 
 // What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
-console.log(challenge.charCodeAt(11))
+console.log(challenge.charCodeAt(11)) // 74
 
 // Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
-console.log(challenge.indexOf('a'))
+console.log(challenge.indexOf('a')) // 4
 
 // Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
-console.log(challenge.lastIndexOf('a'))
+console.log(challenge.lastIndexOf('a')) // 14
 
 let sentence='You cannot end a sentence with because because because is a conjunction'
 
 // Use indexOf to find the position of the first occurrence of the word because in the sentence:
-console.log(sentence.indexOf('because'))
+console.log(sentence.indexOf('because')) // 31
 
 // Use lastIndexOf to find the position of the last occurrence of the word because in the sentence:
-console.log(sentence.lastIndexOf('because'))
+console.log(sentence.lastIndexOf('because'))  // 47
 
 // Use search to find the position of the first occurrence of the word because in the sentence:
-console.log(sentence.search('because'))
+console.log(sentence.search('because'))  // 31
 
 // Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
 let Str=' 30 Days Of JavaScript '
-console.log(Str.trim())
+console.log(Str.trim())  // 30 Days Of JavaScript
 
 // Use startsWith() method with the string 30 Days Of JavaScript and make the result true
-console.log(challenge.startsWith('30'))
+console.log(challenge.startsWith('30')) // true
 
 // Use endsWith() method with the string 30 Days Of JavaScript and make the result true
-console.log(challenge.endsWith('Script'))
+console.log(challenge.endsWith('Script')) // true
 
 // Use match() method to find all the a’s in 30 Days Of JavaScript
-console.log(challenge.match(/a/g))
+console.log(challenge.match(/a/g)) // [ 'a', 'a', 'a' ]
 
 // Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
 let firstHalf='30 Days of'
-console.log(firstHalf.concat('JavaScript'))
+console.log(firstHalf.concat('JavaScript')) // 30 Days ofJavaScript
 
 // Use repeat() method to print 30 Days Of JavaScript 2 times
-console.log(challenge.repeat(2))
+console.log(challenge.repeat(2)) // 30 Days Of JavaScript30 Days Of JavaScript
 
 /* Using console.log() print out the following statement:
 The quote 'There is no exercise better for the heart than reaching down and lifting people up.'
@@ -99,8 +99,8 @@ console.log(parseFloat('9.8')==10)
 console.log(Math.ceil(parseFloat('9.8'))==10)
 
 // Check if 'on' is found in both python and jargon
-console.log('python'.includes('on'))
-console.log('jargon'.includes('on'))
+console.log('python'.includes('on')) // true
+console.log('jargon'.includes('on')) // true
 
 // I hope this course is not full of jargon. Check if jargon is in the sentence.
 console.log('I hope this course is not full of jargon.'.includes('jargon'))
@@ -131,4 +131,8 @@ console.log("2\t1\t2\t4\t8\t")
 console.log("3\t1\t3\t9\t27\t")
 console.log("4\t1\t4\t16\t64\t")
 console.log("5\t1\t5\t25\t125\t")
+
+
+
+
 
